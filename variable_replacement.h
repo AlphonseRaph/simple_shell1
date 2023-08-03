82309df9213fdfee7e550deacf05d0c0bd6c1e3f
AlphonseRaph <111273955+AlphonseRaph@users.noreply.github.com>
2023-08-03 03:19:58 +0300

    Delete variable_replacement.h

--- variable_replacement.h
+++
@@ -1,7 +0,0 @@
-#ifndef VARIABLE_REPLACEMENT_H
-#define VARIABLE_REPLACEMENT_H
-
-// Function to replace variables in the command
-void replace_variables(char *command);
-
-#endif /* VARIABLE_REPLACEMENT_H */
